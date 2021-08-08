5a6a403844d744fe99dda5f8e19f811e9707871f
natalia <mellomartins.naty@gmail.com>
2021-08-07 21:08:18 -0300

    req 7

--- src/zoo.js
+++ src/zoo.js
@@ -1,24 +1,29 @@
+const { species } = require('./data');
 const data = require('./data');
 
+/* ~~~~~~~ REQUISITO 1 ~~~~~~~ */
 function getSpeciesByIds(...ids) {
-  return data.species.filter((specie) => ids.includes(specie.id));
+  return data.species.filter(specie => ids.includes(specie.id));
 }
 // Consultei o reposotório da aluna Ariane Ueti para acertar detalhes da função.
 // link: https://github.com/tryber/sd-014-b-project-zoo-functions/pull/68/files
 
+/* ~~~~~~~ REQUISITO 2 ~~~~~~~ */
 function getAnimalsOlderThan(animal, age) {
-  const animalsList = data.species.find((specie) => specie.name === animal);
-  return animalsList.residents.every((resident) => resident.age > age);
+  const animalsList = data.species.find(specie => specie.name === animal);
+  return animalsList.residents.every(resident => resident.age > age);
 }
 // Consultei o reposotório do aluno Leomar Linhares para acertar detalhes da função.
 // link: https://github.com/tryber/sd-014-b-project-zoo-functions/pull/75/files
 
+/* ~~~~~~~ REQUISITO 3 ~~~~~~~ */
 function getEmployeeByName(employeeName) {
   const selectedName = data.employees
-    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+    .find(employee => employee.firstName === employeeName || employee.lastName === employeeName);
   return (selectedName === undefined) ? {} : selectedName;
 }
 
+/* ~~~~~~~ REQUISITO 4 ~~~~~~~ */
 function createEmployee(personalInfo, associatedWith) {
   const newEmployee = {
     ...personalInfo,
@@ -27,6 +32,7 @@ function createEmployee(personalInfo, associatedWith) {
   return newEmployee;
 }
 
+/* ~~~~~~~ REQUISITO 5 ~~~~~~~ */
 function isManager(id) {
   //   // array com ids dos employees
   //   const employeesIds = Object.values(data.employees.id);
@@ -34,6 +40,7 @@ function isManager(id) {
 
 }
 
+/* ~~~~~~~ REQUISITO 6 ~~~~~~~ */
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   const newEmployee = {
     id,
@@ -47,30 +54,48 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   return data.employees;
 }
 
-function countAnimals(species) {
-  // seu código aqui
+/* ~~~~~~~ REQUISITO 7 ~~~~~~~ */
+function countAnimals(animalSpecie) {
+  if (animalSpecie === undefined) {
+    return data.species.reduce((accumulator, current) => {
+      (accumulator[current.name] = current.residents.length);
+      return accumulator;
+    }, {});
+} else {
+  const selectedAnimal = data.species.find(specie => specie.name === animalSpecie);
+  return selectedAnimal.residents.length;
+  };
 }
+// Consultei o reposotório do aluno Wellington Fonseca para acertar detalhes da função.
+// link: https://github.com/tryber/sd-014-b-project-zoo-functions/pull/81/files
 
+
+/* ~~~~~~~ REQUISITO 8 ~~~~~~~ */
 function calculateEntry(entrants) {
   // seu código aqui
 }
 
+/* ~~~~~~~ REQUISITO 9 ~~~~~~~ */
 function getAnimalMap(options) {
   // seu código aqui
 }
 
+/* ~~~~~~~ REQUISITO 10 ~~~~~~~ */
 function getSchedule(dayName) {
   // seu código aqui
 }
 
+/* ~~~~~~~ REQUISITO 11 ~~~~~~~ */
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
 }
 
+/* ~~~~~~~ REQUISITO 12 ~~~~~~~ */
 function increasePrices(percentage) {
   // seu código aqui
 }
 
+/* ~~~~~~~ REQUISITO 13 ~~~~~~~ */
 function getEmployeeCoverage(idOrName) {
   // seu código aqui
 }
