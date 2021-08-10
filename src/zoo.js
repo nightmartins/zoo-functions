3ac584f7f8d0ab0e79fc1ec04fa3d95d5218f07b
natalia <mellomartins.naty@gmail.com>
2021-08-10 00:26:54 -0300

    requisito 11 e explicação de algumas funções

--- src/zoo.js
+++ src/zoo.js
@@ -5,7 +5,7 @@ const data = require('./data');
 function getSpeciesByIds(...ids) {
   return data.species.filter((specie) => ids.includes(specie.id));
 }
-// Consultei o reposotório da aluna Ariane Ueti para acertar detalhes da função.
+// Consultei o reposotório da aluna Ariane Ueti para corrigir a escrita da função.
 // link: https://github.com/tryber/sd-014-b-project-zoo-functions/pull/68/files
 
 /* ~~~~~~~ REQUISITO 2 ~~~~~~~ */
@@ -13,7 +13,7 @@ function getAnimalsOlderThan(animal, age) {
   const animalsList = data.species.find((specie) => specie.name === animal);
   return animalsList.residents.every((resident) => resident.age > age);
 }
-// Consultei o reposotório do aluno Leomar Linhares para acertar detalhes da função.
+// Consultei o reposotório do aluno Leomar Linhares para corrigir a escrita da função.
 // link: https://github.com/tryber/sd-014-b-project-zoo-functions/pull/75/files
 
 /* ~~~~~~~ REQUISITO 3 ~~~~~~~ */
@@ -34,14 +34,10 @@ function createEmployee(personalInfo, associatedWith) {
 
 /* ~~~~~~~ REQUISITO 5 ~~~~~~~ */
 function isManager(id) {
-  //   // array com ids dos employees
-  //   const employeesIds = Object.values(data.employees.id);
-  //   // comparar se o .responsibleFor de cada um dos employees contem algum id dos employees
-
 }
 
 /* ~~~~~~~ REQUISITO 6 ~~~~~~~ */
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) { // setando o padrão dos parâmetros como arrays vazios.
   const newEmployee = {
     id,
     firstName,
@@ -56,16 +52,16 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 /* ~~~~~~~ REQUISITO 7 ~~~~~~~ */
 function countAnimals(animalSpecie) {
-  if (animalSpecie === undefined) {
-    return data.species.reduce((accumulator, current) => {
-      (accumulator[current.name] = current.residents.length);
+  if (animalSpecie === undefined) { // caso não seja passado parâmetro, 
+    return data.species.reduce((accumulator, specie) => {
+      accumulator[specie.name] = specie.residents.length; // retornar o nome da espécie do animal e a quantidade de residentes, para todos os residentes
       return accumulator;
-    }, {});
+    }, {}); // em forma de objeto
   }
-  const selectedAnimal = data.species.find((specie) => specie.name === animalSpecie);
-  return selectedAnimal.residents.length;
+  const selectedAnimal = data.species.find((specie) => specie.name === animalSpecie); // caso seja passado um parâmetro, 
+  return selectedAnimal.residents.length; // retornar o número de residentes da espécie selecionada.
 }
-// Consultei o reposotório do aluno Wellington Fonseca para acertar detalhes da função.
+// Consultei o reposotório do aluno Wellington Fonseca para corrigir a escrita da função.
 // link: https://github.com/tryber/sd-014-b-project-zoo-functions/pull/81/files
 
 /* ~~~~~~~ REQUISITO 8 ~~~~~~~ */
@@ -76,6 +72,7 @@ function calculateEntry(entrants) {
   const { Adult = 0, Senior = 0, Child = 0 } = entrants;
   return Adult * prices.Adult + Senior * prices.Senior + Child * prices.Child;
 }
+// Função para verificar se um objeto é vazio encontrada no link: https://pt.stackoverflow.com/questions/83588/em-javascript-como-verificar-que-um-objeto-est%C3%A1-vazio-sem-jquery
 
 /* ~~~~~~~ REQUISITO 9 ~~~~~~~ */
 function getAnimalMap(options) {
@@ -104,9 +101,15 @@ function getSchedule(dayName) {
 
 /* ~~~~~~~ REQUISITO 11 ~~~~~~~ */
 function getOldestFromFirstSpecies(selectedId) {
-// const firstAnimal = data.employees.find((item) => item.id = selectedId);
-// return [firstAnimal.name, firstAnimal.sex, firstAnimal.age];
+  const firstAnimal = data.employees.find((item) => item.id === selectedId).responsibleFor[0]; // seleciona o id da primeira espécie pela qual o empregado buscado é responsável
+  const animalInfo = data.species.find((animal) => animal.id === firstAnimal).residents; // busca a espécie pelo id e retorna seus residentes
+  const orderedAnimals = animalInfo.sort((a, b) => b.age - a.age); // organiza os residentes por idade, de forma decrescente
+  const { name, sex, age } = orderedAnimals[0]; // extrai os valores do objeto do animal na primeira posição
+  return [name, sex, age]; // retorna o objeto como array
 }
+// Consultei o reposotório do aluno Victor Martins para corrigir a escrita da função.
+// link: https://github.com/tryber/sd-014-b-project-zoo-functions/pull/53/files
+// Função para ordenação das idades de forma decrescente encontrada no link: https://ricardo-reis.medium.com/o-m%C3%A9todo-sort-do-array-javascript-482576734e0a
 
 /* ~~~~~~~ REQUISITO 12 ~~~~~~~ */
 function increasePrices(percentage) {
@@ -119,7 +122,6 @@ function increasePrices(percentage) {
 
 /* ~~~~~~~ REQUISITO 13 ~~~~~~~ */
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
 }
 
 module.exports = {
